e8612ada9370b963d39e953e3861dff51f41c1bb
Rody Molenaar <rody.molenaar@xtrada.nl>
2016-05-16 00:32:18 +0200

    Update to 3.0.0

---
+++ materialcraft-demo.js
@@ -0,0 +1,17 @@
+function swapStyleSheet(sheet) {
+  document.getElementById("themestyle").setAttribute("href", sheet);
+}
+
+function initiate() {
+  var standard = document.getElementById("standard");
+  var dark = document.getElementById("dark");
+  var blue = document.getElementById("blue");
+  var red = document.getElementById("red");
+
+  standard.onclick = function () { swapStyleSheet("https://rawgit.com/rodymol123/MaterialCraft/master/themes/MaterialCraft/dist/materialcraft.css") };
+  dark.onclick = function () { swapStyleSheet("https://rawgit.com/rodymol123/MaterialCraft/master/themes/MaterialCraft-Dark/dist/materialcraft.css"); };
+  blue.onclick = function () { swapStyleSheet("https://rawgit.com/rodymol123/MaterialCraft/master/themes/MaterialCraft-Blue/dist/materialcraft.css"); };
+  red.onclick = function () { swapStyleSheet("https://rawgit.com/rodymol123/MaterialCraft/master/themes/MaterialCraft-Red/dist/materialcraft.css"); };
+}
+
+window.onload = initiate;
