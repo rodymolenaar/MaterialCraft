fa78b7502c4aae6aeccaa913b81846f83230f1d9
Rody Molenaar <rody.molenaar@xtrada.nl>
2016-05-16 00:37:22 +0200

    Use CDN

--- materialcraft-demo.js
+++ materialcraft-demo.js
@@ -8,10 +8,10 @@ function initiate() {
   var blue = document.getElementById("blue");
   var red = document.getElementById("red");
 
-  standard.onclick = function () { swapStyleSheet("https://rawgit.com/rodymol123/MaterialCraft/master/themes/MaterialCraft/dist/materialcraft.css") };
-  dark.onclick = function () { swapStyleSheet("https://rawgit.com/rodymol123/MaterialCraft/master/themes/MaterialCraft-Dark/dist/materialcraft.css"); };
-  blue.onclick = function () { swapStyleSheet("https://rawgit.com/rodymol123/MaterialCraft/master/themes/MaterialCraft-Blue/dist/materialcraft.css"); };
-  red.onclick = function () { swapStyleSheet("https://rawgit.com/rodymol123/MaterialCraft/master/themes/MaterialCraft-Red/dist/materialcraft.css"); };
+  standard.onclick = function () { swapStyleSheet("https://cdn.rawgit.com/rodymol123/MaterialCraft/master/themes/MaterialCraft/dist/materialcraft.css") };
+  dark.onclick = function () { swapStyleSheet("https://cdn.rawgit.com/rodymol123/MaterialCraft/master/themes/MaterialCraft-Dark/dist/materialcraft.css"); };
+  blue.onclick = function () { swapStyleSheet("https://cdn.rawgit.com/rodymol123/MaterialCraft/master/themes/MaterialCraft-Blue/dist/materialcraft.css"); };
+  red.onclick = function () { swapStyleSheet("https://cdn.rawgit.com/rodymol123/MaterialCraft/master/themes/MaterialCraft-Red/dist/materialcraft.css"); };
 }
 
 window.onload = initiate;
