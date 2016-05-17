f11443d5c536085c1dcf749a7c5824e0c4709e12
Rody Molenaar <rody.molenaar@xtrada.nl>
2016-05-17 20:48:20 +0200

    Add Purple flavour

--- materialcraft-demo.js
+++ materialcraft-demo.js
@@ -4,13 +4,15 @@ function swapStyleSheet(sheet) {
 
 function initiate() {
   var standard = document.getElementById("standard");
-  var dark = document.getElementById("dark");
   var blue = document.getElementById("blue");
+  var dark = document.getElementById("dark");
+  var purple = document.getElementById("purple");
   var red = document.getElementById("red");
 
   standard.onclick = function () { swapStyleSheet("https://cdn.rawgit.com/rodymol123/MaterialCraft/master/themes/MaterialCraft/dist/materialcraft.css") };
-  dark.onclick = function () { swapStyleSheet("https://cdn.rawgit.com/rodymol123/MaterialCraft/master/themes/MaterialCraft-Dark/dist/materialcraft.css"); };
   blue.onclick = function () { swapStyleSheet("https://cdn.rawgit.com/rodymol123/MaterialCraft/master/themes/MaterialCraft-Blue/dist/materialcraft.css"); };
+  dark.onclick = function () { swapStyleSheet("https://cdn.rawgit.com/rodymol123/MaterialCraft/master/themes/MaterialCraft-Dark/dist/materialcraft.css"); };
+  purple.onclick = function () { swapStyleSheet("https://cdn.rawgit.com/rodymol123/MaterialCraft/master/themes/MaterialCraft-Purple/dist/materialcraft.css"); };
   red.onclick = function () { swapStyleSheet("https://cdn.rawgit.com/rodymol123/MaterialCraft/master/themes/MaterialCraft-Red/dist/materialcraft.css"); };
 }
 
