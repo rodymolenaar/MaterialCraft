6684ab721588ee5aab1bc17a0188c52fe0ed039b
Rody Molenaar <rody.molenaar@xtrada.nl>
2016-12-06 12:08:29 +0100

    More demonstration

--- materialcraft-demo.js
+++ materialcraft-demo.js
@@ -11,13 +11,13 @@ function initiate() {
   var green = document.getElementById("green");
   var yellow = document.getElementById("yellow");
 
-  standard.onclick = function () { swapStyleSheet("https://cdn.rawgit.com/rodymol123/MaterialCraft/master/dist/materialcraft.css") };
-  blue.onclick = function () { swapStyleSheet("https://cdn.rawgit.com/rodymol123/MaterialCraft/master/dist/materialcraft-blue.css"); };
-  dark.onclick = function () { swapStyleSheet("https://cdn.rawgit.com/rodymol123/MaterialCraft/master/dist/materialcraft-dark.css"); };
-  purple.onclick = function () { swapStyleSheet("https://cdn.rawgit.com/rodymol123/MaterialCraft/master/dist/materialcraft-purple.css"); };
-  red.onclick = function () { swapStyleSheet("https://cdn.rawgit.com/rodymol123/MaterialCraft/master/dist/materialcraft-red.css"); };
-  green.onclick = function () { swapStyleSheet("https://cdn.rawgit.com/rodymol123/MaterialCraft/master/dist/materialcraft-green.css"); };
-  yellow.onclick = function () { swapStyleSheet("https://cdn.rawgit.com/rodymol123/MaterialCraft/master/dist/materialcraft-yellow.css"); };
+  standard.onclick = function () { swapStyleSheet("https://rawgit.com/rodymol123/MaterialCraft/master/dist/materialcraft.css") };
+  blue.onclick = function () { swapStyleSheet("https://rawgit.com/rodymol123/MaterialCraft/master/dist/materialcraft-blue.css"); };
+  dark.onclick = function () { swapStyleSheet("https://rawgit.com/rodymol123/MaterialCraft/master/dist/materialcraft-dark.css"); };
+  purple.onclick = function () { swapStyleSheet("https://rawgit.com/rodymol123/MaterialCraft/master/dist/materialcraft-purple.css"); };
+  red.onclick = function () { swapStyleSheet("https://rawgit.com/rodymol123/MaterialCraft/master/dist/materialcraft-red.css"); };
+  green.onclick = function () { swapStyleSheet("https://rawgit.com/rodymol123/MaterialCraft/master/dist/materialcraft-green.css"); };
+  yellow.onclick = function () { swapStyleSheet("https://rawgit.com/rodymol123/MaterialCraft/master/dist/materialcraft-yellow.css"); };
 }
 
 window.onload = initiate;
